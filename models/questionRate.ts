3406ca1e27821464d1869d13a1c83633791a4342
melotek <pawlaczyk.lukasz@gmail.com>
2024-03-19 20:21:50 +0100

    deply

--- models/questionRate.ts
+++ models/questionRate.ts
@@ -1,14 +1,16 @@
-import type {IquestionRating} from '@/types.d.ts'
-import {Schema, model, Types} from 'mongoose';
+import type { IquestionRating } from "@/types";
+import { Schema, model, Types } from "mongoose";
 
 export const questionRateSchema = new Schema<IquestionRating>({
-    userId: { type: Types.ObjectId, ref: 'User' },
-    questionId: {type: Types.ObjectId, ref:'questionForm'},
-    rating: [{ 
-        userId: { type: Types.ObjectId, ref: 'User' },
-        questionId: { type: Types.ObjectId, ref: 'question' },
-        rating: Number
-    }]
+  userId: { type: Types.ObjectId, ref: "User" },
+  questionId: { type: Types.ObjectId, ref: "questionForm" },
+  rating: [
+    {
+      userId: { type: Types.ObjectId, ref: "User" },
+      questionId: { type: Types.ObjectId, ref: "question" },
+      rating: Number,
+    },
+  ],
 });
-const questionRate = model<IquestionRating>('questionRate', questionRateSchema);
+const questionRate = model<IquestionRating>("questionRate", questionRateSchema);
 export default questionRate;
--- models/rating.ts
+++ models/rating.ts
@@ -1,13 +1,13 @@
-import type { IRating } from '@/types.d.ts'
-import {Schema, model, Types} from 'mongoose';
+import type { IRating } from "@/types";
+import { Schema, model, Types } from "mongoose";
 
 export const ratingSchema = new Schema<IRating>({
-    userId: Types.ObjectId,
-    questionId: Types.ObjectId,
-    rating: {
-        type: Number,
-        required: true,
-    },
-})
-const Rating = model<IRating>('Rating', ratingSchema);
+  userId: Types.ObjectId,
+  questionId: Types.ObjectId,
+  rating: {
+    type: Number,
+    required: true,
+  },
+});
+const Rating = model<IRating>("Rating", ratingSchema);
 export default Rating;
