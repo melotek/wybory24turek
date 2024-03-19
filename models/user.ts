a303b9dd385c5a23dcf00d9953c4d5972a43169c
melotek <pawlaczyk.lukasz@gmail.com>
2024-03-19 20:24:49 +0100

    deply

--- models/user.ts
+++ models/user.ts
@@ -1,27 +1,27 @@
-import type {IUser} from '@/types.d.ts'
-import {Schema, model, Types} from 'mongoose';
- 
+import type { IUser } from "@/types";
+import { Schema, model, Types } from "mongoose";
+
 export const userSchema = new Schema<IUser>({
-    firstname: {
-        type: String,
-        required: true,
-    },
-    useremail: {
-        type: String,
-        required: true,
-    },
-    email: {
-        type: String,
-        required: true,
-    },
-    password: {
-        type: String,
-        required: true,
-    },
-    role: {
-        type: String,
-        required: true,
-    },
-})
-const User = model<IUser>('User', userSchema);
+  firstname: {
+    type: String,
+    required: true,
+  },
+  useremail: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    required: true,
+  },
+});
+const User = model<IUser>("User", userSchema);
 export default User;
